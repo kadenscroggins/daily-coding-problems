ee3da72afb3d9cf4525da03367bc469d7a9d4940
= <=>
2022-05-31 13:36:20 -0500

    Modified to not create trailing spaces

--- 2022-05-30/solution.py
+++ 2022-05-30/solution.py
@@ -4,29 +4,29 @@ class Node:
         self.left = left
         self.right = right
 
+    # Serialize binary tree into string using preorder traversal
     def serialize(self):
-        # This function user Preorder Traversal
-
-        ret = str(self.val) + " "
+        ret = str(self.val)
 
         if isinstance(self.left, Node):
-            ret += self.left.serialize()
+            ret += " " + self.left.serialize()
         elif self.left is None:
-            ret += "None "
-        else: ret += str(self.left.val) + " "
+            ret += " None"
+        else: ret += " " + str(self.left.val)
 
         if isinstance(self.right, Node):
-            ret += self.right.serialize()
+            ret += " " + self.right.serialize()
         elif self.right is None:
-            ret += "None "
-        else: ret += str(self.right.val) + " "
+            ret += " None"
+        else: ret += " " + str(self.right.val)
 
         return ret
 
+    # Deserialize string into binary tree using preorder traversal
     def deserialize(val):
         pass
 
-node = Node('root', Node('left', Node('left.left')), Node('right'))
+node = Node(1, Node(2, Node(4)), Node(3))
 #assert deserialize(serialize(node)).left.left.val == 'left.left'
 
 print(node.serialize())
