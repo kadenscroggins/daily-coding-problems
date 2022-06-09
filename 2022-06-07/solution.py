c3df1985af6f96c3697d5ceeea5564bc986d4699
scroggik <scroggik@nsuok.edu>
2022-06-09 14:11:01 -0500

    Solved

--- 2022-06-07/solution.py
+++ 2022-06-07/solution.py
@@ -1,3 +1,10 @@
+'''
+This solution is quick and effective.
+For a huge dictionary and longer queries, it would be
+more efficient to sort the set of strings into a tree where
+each node is a letter A-Z so that a query could just go
+down the tree to find if there is a string match.
+'''
 import re
 
 QUERY = 'de'
@@ -5,6 +12,7 @@ STRINGS = ['dog', 'deer', 'deal']
 
 processed = []
 for s in STRINGS:
+    # This regex checks for QUERY at the beginning of s
     if re.search(f'\\A{QUERY}', s) is None:
         pass
     else:
