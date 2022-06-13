f531ab733c66444e4155eed7f5de82d401038027
scroggik <scroggik@nsuok.edu>
2022-06-13 13:51:11 -0500

    Solved

---
+++ 2022-06-11/solution.py
@@ -0,0 +1,18 @@
+from random import randint
+
+count = 0
+with open('./lines.txt') as file:
+    for line in file:
+        count += 1
+
+index = randint(0, count)
+
+count = 0
+with open('lines.txt') as file:
+    for line in file:
+        if index != count:
+            pass
+        else:
+            print(f'Line {count}: {line}')
+            break
+        count += 1
