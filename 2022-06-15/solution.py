0055e9d7c262c90f1aabbb7e89a63f2ea7f928be
scroggik <scroggik@nsuok.edu>
2022-06-17 16:04:25 -0500

    Solved

--- 2022-06-15/solution.py
+++ 2022-06-15/solution.py
@@ -10,12 +10,17 @@ for i in range(len(HOUSES[0])):
     temp_cost = HOUSES[0][i]
     left_house = i
     
-    for j in range(len(HOUSES)):
-        if j == left_house:
-            continue
-        else:
-            temp_cost 
-###
+    for j in range(1, len(HOUSES)):
+        temp_lowest = -1
+        for k in range(len(HOUSES[j])):
+            if k == left_house:
+                continue
+            elif temp_lowest == -1:
+                temp_lowest = HOUSES[j][k]
+            elif HOUSES[j][k] < temp_lowest:
+                temp_lowest = HOUSES[j][k]
+                left_house = k
+        temp_cost += temp_lowest
 
     costs.append(temp_cost)
 print(min(costs))
